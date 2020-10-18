f841ee346a16111fafc16f688f0f514d9c17ea6d
Ben Pyrik <ben@wsundine.com>
2020-10-17 20:38:20 -0600

    Fetch liked & lost item data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from "react";
 
-function App() {
+import { useAsync } from "./hooks/useAsync";
+import fetchItemData from "./utilities/fetchItemData";
+
+const LoadingApp = () => {
+  return (
+    <div className="helvetica">
+      <p>Loading</p>
+    </div>
+  );
+};
+
+const ErrorApp = (props) => {
+  console.error(props.error);
+  return (
+    <div className="helvetica">
+      <p>Error :(</p>
+    </div>
+  );
+};
+
+const SuccessApp = (props) => {
+  const { data } = props;
+  console.log(data);
   return (
     <div className="helvetica">
       <p>TODO</p>
     </div>
   );
+};
+
+function App() {
+  const { status, value, error } = useAsync(fetchItemData, true);
+
+  if (status === "idle" || status === "pending") {
+    return <LoadingApp />;
+  }
+
+  if (status === "error") {
+    return <ErrorApp error={error} />;
+  }
+
+  return <SuccessApp data={value} />;
 }
 
 export default App;
---
+++ src/utilities/fetchItemData.js
@@ -0,0 +1,16 @@
+const likedItemsPath = `${process.env.PUBLIC_URL}/data/liked-items.json`;
+const lostItemsPath = `${process.env.PUBLIC_URL}/data/lost-items.json`;
+
+const fetchAndParse = (url) => fetch(url).then((result) => result.json());
+
+export default async function fetchItemData() {
+  const urlsToFetch = [likedItemsPath, lostItemsPath];
+
+  const promisedResponses = [];
+  for (let url of urlsToFetch) {
+    const promisedResponse = fetchAndParse(url);
+    promisedResponses.push(promisedResponse);
+  }
+  const responses = await Promise.all(promisedResponses);
+  return { likedItems: responses[0], lostItems: responses[1] };
+}
