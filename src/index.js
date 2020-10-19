26e0cec8e7eabb15f4dda1732a18ee9b555ed54d
Ben Pyrik <ben@wsundine.com>
2020-10-18 18:00:41 -0600

    Add Sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import "./index.css";
 import ErrorBoundary from "./components/ErrorBoundary";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+Sentry.init({
+  dsn:
+    "https://002fe4b3d66143ce846a75ad57fd0d12@o260768.ingest.sentry.io/5468516",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
